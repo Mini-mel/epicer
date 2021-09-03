a810ebce5a8dd1088fe1e6e316e736de682bd49e
Mini Mel <25712940+Mini-mel@users.noreply.github.com>
2021-09-02 17:03:13 -0700

    add button as scaffolding for dynamic ingredients field in add new recipe form

---
+++ public/scripts/addField.js
@@ -0,0 +1,7 @@
+document.getElementById("addFieldButton").addEventListener("click", function (e) {
+    e.preventDefault();
+})
+
+function addField() {
+    console.log("adds fields to ingredients");
+}
