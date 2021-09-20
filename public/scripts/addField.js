f3626067a3bf8cfd885be26429f67ac2343519bc
Mini Mel <25712940+Mini-mel@users.noreply.github.com>
2021-09-20 11:17:27 -0700

    Add multiple ingredients when creating recipe

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -7,7 +7,25 @@ let ingredientsCounter = 0;
 function addField() {
     if (ingredientsCounter < 20) {
         document.getElementById("add-field-div").insertAdjacentHTML('afterend',
-            '<div id="ingredient-line" class="added-field"><input type="text" class="ingredient" id="ingredients" name="recipe[ingredients[' + ingredientsCounter + '].name]" aria-label="ingredient" required><input type = "number" value = "1" min = "0.1" max = "100" step = "0.1" name="recipe[ingredients[0].amount]" aria-label="number of ingredient"><select name="recipe[ingredients[0].unit]" aria-label="units of measurement"><option></option><option>To taste</option><option>cup(s)</option><option>tsp(s)</option><option>tbsp(s)</option><option>gallon(s)</option><option>quart(s)</option></select></div > ')
+            '<div id="ingredient-line" class="added-field">'
+
+            + '<input type="text" class="ingredients" id="ingredients" name="recipe[ingredients][' + ingredientsCounter + '][name]"'
+            + 'aria-label="ingredient" required>'
+
+            + '<input type="number" value="1" min="0.1" max="100" step="0.1" name="recipe[ingredients][' + ingredientsCounter + '][amount]"'
+            + 'aria-label="amount of ingredient">'
+
+            + '<select name="recipe[ingredients][' + ingredientsCounter + '][unit]" aria-label="units of measurement">'
+            + '<option></option>'
+            + '<option>To taste</option>'
+            + '<option>cup(s)</option>'
+            + '<option>tsp(s)</option>'
+            + '<option>tbsp(s)</option>'
+            + '<option>gallon(s)</option>'
+            + '<option>quart(s)</option>'
+            + '</select>'
+
+            + '</div>')
         ingredientsCounter++;
     }
 }
