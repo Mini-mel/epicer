92c19e63d35074f612f93717207758d8fbce178f
Mini Mel <25712940+Mini-mel@users.noreply.github.com>
2023-10-30 17:50:52 -0700

    add put route, edit page, functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
 const Recipe = require("./models/recipe");
+const methodOverride = require("method-override");
 
 //--------Connect to Database--------
 mongoose.connect("mongodb://localhost:27017/epicer", {
@@ -25,6 +26,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //--------Routes--------
 app.get("/recipes", async (req, res) => {
@@ -52,6 +54,13 @@ app.get("/recipes/:id", async (req, res) => {
 app.get("/recipes/:id/edit", async (req, res) => {
   const recipe = await Recipe.findById(req.params.id);
   res.render("recipes/edit", { recipe });
+});
+
+app.put("/recipes/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(req.body);
+  const recipe = await Recipe.findByIdAndUpdate(id, { ...req.body.recipe });
+  res.redirect(`/recipes/${recipe._id}`);
 });
 
 app.get("/", (req, res) => {
